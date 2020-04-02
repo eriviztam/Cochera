461f9897f85bf11a4d73b0ecf37ea2a7e2f94975
eriviztam <61073665+eriviztam@users.noreply.github.com>
2020-04-01 22:44:07 -0600

    Create index.jsx

---
+++ src/Componentes/Calendarizacion/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import { Column } from '../Column'
+import InitialState from '../../Utils/Mokcup.json'
+import './styles.css'
+
+export const Calendarizacion = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        setData(InitialState)
+    }, [data])
+
+    return (
+        <div className="Calendarizacion">
+            {
+                data.map(item => <Column {...item} key={item.id} />)
+            }
+        </div>
+    )
+} 
