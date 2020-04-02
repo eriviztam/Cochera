39e23a40392eef56e6406e85036e2506312cdd01
eriviztam <61073665+eriviztam@users.noreply.github.com>
2020-04-01 19:40:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import './App.css';
+import { Layout } from './Components/Layout'
+import { Calendarizacion } from './Components/Calendarizacion'
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-     <h1 className="title">
-     Hello world <span aria-label="emoji" role="img">🔥</span>
-     </h1>
-     <input type=" text" className="new-task"/>
+    <div className="App">
+    <Layout>
+      <Calendarizacion/>
+    </layout>
+>
     </div>
   );
 }
 
-export default App;
+
